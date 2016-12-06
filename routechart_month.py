3e4efa3b3d889e2570c331e28c9378c54fc3230b
ITSupapitch <59070169@it.kmitl.ac.th>
2016-12-06 23:46:05 +0700

    edited route chart of the month
    แกไข docstring , title และ เพิ่ม labels

--- routechart_month.py
+++ routechart_month.py
@@ -1,14 +1,14 @@
 """ Statistics delays of trains Thailand Project
-    This program shows chart of September month 2016 that show statistics average delays of trains Thailand divided by route
+    This program shows chart of September 2016 that show statistics average delays of Thai trains divided by route
 """
 import pygal
 def make_chart():
-    """This function use to make chart of September month 2016 that show statistics average delays
-    of trains Thailand divided by route"""
+    """This function use to make chart of September 2016 that show statistics average delays
+    of Thai trains divided by route"""
     from pygal.style import NeonStyle
     chart = pygal.StackedLine(fill=True, interpolate='cubic', style=NeonStyle)
-    line_chart = pygal.StackedLine(fill=True)
-    line_chart.title = 'Statistics average delays of trains Thailand divided by route of September month 2016'
+    chart.title = 'Statistics average delays of Thai trains divided by route of September 2016'
+    chart.x_labels = map(str, range(1, 31))
     chart.add('Northern Line', [15.26, 12.72, 12.47, 14.89, 20.69, 23.33, 21.67, 19.40, 8.71, 8.00, 7.37, 11.66, 12.81, 16.36, 11.17, 10.55,
                 29.42, 13.75, 18.98, 16.43, 14.49, 11.10, 14.36, 11.58, 9.22, 10.62, 9.36, 16.56, 11.64, 18.76])
     chart.add('North Eastern Line', [14.44, 8.40, 13.94, 7.12, 13.15, 14.64, 10.62, 9.42, 14.81, 7.85, 7.79, 9.50, 11.43, 12.76, 15.50, 13.89,
